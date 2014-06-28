ecef3edc42be0d7ba6a9450976558828f07c8997
starand <starand@ukr.net>
2014-06-28 09:02:59 -0700

    SIte: Fixed issue with invalid characters in session_id()

--- site/files/common.php
+++ site/files/common.php
@@ -1,6 +1,26 @@
 <?
-	session_start();
+	function my_session_start()
+	{
+		if (isset($_COOKIE['PHPSESSID'])) {
+			$sessid = $_COOKIE['PHPSESSID'];
+		} else if (isset($_GET['PHPSESSID'])) {
+			$sessid = $_GET['PHPSESSID'];
+		} else {
+			session_start();
+			return false;
+		}
+
+		if (!preg_match('/^[a-z0-9]{32}$/', $sessid)) {
+			return false;
+		}
+		session_start();
+
+	   return true;
+	}
+
+	my_session_start();
 	header('Content-Type: text/html; charset=windows-1251');
+	
 	define ("COMMON", 1);
 	
 	include_once "db.php"; // connecting to db
